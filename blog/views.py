a7d3879602e1037bed452225edc4be806bd6cc4d
Sharique Ansari <sharique098@gmail.com>
2019-07-20 02:47:52 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,7 @@ def home(request):
 
 
 def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
